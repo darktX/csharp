b5a7559119362270c5bf3e2a8cf66241a8d5168f
darktX <robotnr4@gmail.com>
2016-11-26 13:55:10 +0100

    Added InsertSort

---
+++ InsertSort/InsertSort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int l = 0;
+            Console.WriteLine("Wpisz ile miejsc ma mieć tablica: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] tab = new int[n];
+            //wpisywanie do tablicy
+            for(int i =0;i<n;i++)
+            {
+                tab[i] = Convert.ToInt32(Console.ReadLine());
+                
+
+            }
+            // sortowanie przez wstawianie 
+         for(int k=1;k<n;k++)
+            {
+                l = tab[k];
+                //ustalone k>=1
+                while(k>0&&l<tab[k-1])
+                {
+                    tab[k] = tab[k - 1];
+                    k--;
+                }
+                tab[k] = l;
+            }
+            //wypisywanie z tablicy
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(tab[i]);
+
+
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
