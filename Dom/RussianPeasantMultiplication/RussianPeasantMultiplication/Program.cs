736d393dd0511617ba92a6788e0da0764ed4850e
darktX <robotnr4@gmail.com>
2016-11-26 15:50:10 +0100

    added new file

---
+++ Dom/RussianPeasantMultiplication/RussianPeasantMultiplication/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RussianPeasantMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wynik = 0;
+            Console.WriteLine("Wprowadź a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wprowadź b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            // algorytm mnożenia rosyjskich chłopów
+            while (b != 1)
+            {
+                if (b % 2 != 0)
+                {
+                    //b jest liczbą nieparzystą
+                    wynik = wynik + a;
+                }
+                a = a * 2;
+                b = b / 2;
+            }
+            wynik = wynik + a;
+            Console.WriteLine($"Wynik to: {wynik}");
+            Console.ReadKey();
+
+        }
+    }
+}
