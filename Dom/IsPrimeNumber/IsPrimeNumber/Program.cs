e57b80e4a023b7ed9b3f5b48dbf409d23492c7da
darktX <robotnr4@gmail.com>
2016-11-28 00:11:56 +0100

    Added GetRemainder method

--- Dom/IsPrimeNumber/IsPrimeNumber/Program.cs
+++ Dom/IsPrimeNumber/IsPrimeNumber/Program.cs
@@ -8,6 +8,28 @@ namespace IsPrimeNumber
 {
     class Program
     {
+        static public int GetRemainder(int a1,int b1)
+        {
+            int result, b2;
+            int preva = 0;
+            b2 = b1;
+            while(a1>b2)
+            {
+                preva = b2;
+                b2 = b2 + b1;
+
+            }
+            if(a1==b2)
+            {
+                result = 0;
+                return result;
+            }
+            else
+            {
+                result = a1 - preva;
+                return result;
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Wpisz liczbę");
@@ -32,6 +54,9 @@ namespace IsPrimeNumber
             {
                 Console.WriteLine($"Liczba {n} nie jest liczbą pierwszą");
             }
+            n = Convert.ToInt32(Console.ReadLine());
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(GetRemainder(n,i));
             Console.ReadKey();
         }
     }
